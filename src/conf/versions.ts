8ee9816edd1610cd83d4b5564a41a87b1292bd2f
Davide Compagnone <davide.compagnone@eidosmedia.com>
2025-02-20 15:31:55 +0100

    NEON-907 - poc startup

---
+++ src/conf/versions.ts
@@ -0,0 +1,5 @@
+export const VERSIONS: string[] = [
+    "NEON.2024.12.ea1-SNAPSHOT",
+    "NEON.2024.13.ea1-SNAPSHOT",
+    "NEON.2024.14.ea1-SNAPSHOT"
+];
--- src/test/utilities/NeonConnection.test.tsx
+++ src/test/utilities/NeonConnection.test.tsx
@@ -1,7 +1,8 @@
-import { describe, expect, test } from "@jest/globals";
+import { describe, expect, test, jest } from "@jest/globals";
 import { NeonConnection } from "../../utilities/NeonConnection";
 import fs from "fs";
 import path from "path";
+import { Site } from "../../types/site";
 
 const connection = new NeonConnection({
   frontOfficeServiceKey: "yourServiceKey",
@@ -21,5 +22,72 @@ describe("NeonConnection test module.", () => {
       );
       expect(result[0].summary).toBe("Sample story by cfg by code");
     });
+  });
+});
+
+describe("NeonConnection startup tests", () => {
+  test("should throw error for incompatible backend version", async () => {
+    const mockBackendInfo =
+        jest.fn().mockImplementation(() =>
+            ({version: "1.0.0", type: "SITE", state: "RUNNING"}));
+
+    connection.getBackendInfo = mockBackendInfo as jest.MockedFunction<() => Promise<{
+      version: string,
+      type: string,
+      state: string
+    }>>;
+
+    await expect(connection.startup()).rejects.toThrow("Incompatible backend version: 1.0.0");
+  });
+
+  test("should throw error when sites list is empty", async () => {
+    const mockBackendInfo =
+        jest.fn().mockImplementation(() =>
+            ({version: "NEON.2024.12.ea1-SNAPSHOT", type: "SITE", state: "RUNNING"}));
+
+    const mockLiveSitesList = jest.fn().mockImplementation(() => ([]));
+
+    connection.getBackendInfo = mockBackendInfo as jest.MockedFunction<() => Promise<{
+      version: string,
+      type: string,
+      state: string
+    }>>;
+
+    connection.getLiveSitesList = mockLiveSitesList as jest.MockedFunction<() => Promise<Site[]>>;
+
+    await expect(connection.startup()).rejects.toThrow("Failed to retrieve sites data.");
+  });
+
+  test("connection startup ok", async () => {
+    const site = {
+      root: {
+        id: "root-123",
+        name: "Home",
+        nodeType: "root",
+        title: "Homepage",
+        uri: "/",
+        path: "/",
+        hostname: "www.example.com",
+        previewHostname: "preview.example.com",
+        items: []
+      }
+    };
+
+    const mockBackendInfo =
+        jest.fn().mockImplementation(() =>
+            ({version: "NEON.2024.12.ea1-SNAPSHOT", type: "SITE", state: "RUNNING"}));
+
+    const mockLiveSitesList =
+        jest.fn().mockImplementation(() => ([site]));
+
+    connection.getBackendInfo = mockBackendInfo as jest.MockedFunction<() => Promise<{
+      version: string,
+      type: string,
+      state: string
+    }>>;
+
+    connection.getLiveSitesList = mockLiveSitesList as jest.MockedFunction<() => Promise<Site[]>>;
+
+    await expect(connection.startup()).resolves.not.toThrow();
   });
 });
