e4fab33b84a49c233e8cf128490a455ed7f0d50c
Valerio Cassani <valerio.cassani@eidosmedia.com>
2025-06-19 14:22:29 +0200

    fixed import type

--- src/services/sites.ts
+++ src/services/sites.ts
@@ -1,20 +1,15 @@
 import settings from '../commons/settings';
-import { Site, Menu, LiveBlogPost } from '../types/site';
+import { Site, Menu } from '../types/site';
+import { LiveBlogPost } from '../types/content';
 import { makeRequest } from '../utilities/http-client';
 import logger from '../utilities/logger';
 import { AuthenticatedRequestOptions } from '../types/base';
 
-export async function loadSites({
-  sitemap,
-  viewStatus,
-}: {
-  sitemap: boolean;
-  viewStatus: string;
-}) {
+export async function loadSites({ sitemap, viewStatus }: { sitemap: boolean; viewStatus: string }) {
   const sites = await makeRequest({
     url: `/api/sites/${viewStatus}?${new URLSearchParams({
       siteMap: sitemap.toString(),
-    }).toString()}`
+    }).toString()}`,
   });
 
   const sitesWithSitemap = await Promise.all(
@@ -22,32 +17,30 @@ export async function loadSites({
       ...site,
       logoUrl: await fetchLiveLogoUrl(site),
       viewStatus: `${viewStatus}`,
-      menus: await fetchLiveMenus(site)
+      menus: await fetchLiveMenus(site),
     }))
   );
 
   return sitesWithSitemap;
 
   async function fetchLiveLogoUrl(site: Site) {
-    try{
+    try {
       // i cannot load the preview logo because is not accessible without auth
-      // so manage the to ask logoUrl only for live sites also for preview 
+      // so manage the to ask logoUrl only for live sites also for preview
       return await getLogoUrl(site.root.id, site.apiHostnames.liveHostname);
-    } catch(e){
+    } catch (e) {
       logger.warn(`logo not found for site ${site.root.name}, view status site.viewStatus: ${viewStatus} error: ${e}`);
       return '';
     }
   }
 
   async function fetchLiveMenus(site: Site) {
-    try{
-            // i cannot load the preview logo because is not accessible without auth
-      // so manage the to ask getMenu only for live sites also for preview 
-      const menus = await getMenu(
-          site.apiHostnames.liveHostname
-      );
+    try {
+      // i cannot load the preview logo because is not accessible without auth
+      // so manage the to ask getMenu only for live sites also for preview
+      const menus = await getMenu(site.apiHostnames.liveHostname);
 
-            // Try to find possible keys containing menu items
+      // Try to find possible keys containing menu items
       if (menus && typeof menus === 'object') {
         // Log all keys and their values
         Object.keys(menus).forEach(key => {
@@ -65,7 +58,7 @@ export async function loadSites({
 
             if (node && node.url) {
               item.url = node.url;
-            } 
+            }
           }
           // Recursively handle children items
           if (item.items && Array.isArray(item.items)) {
@@ -75,8 +68,7 @@ export async function loadSites({
       }
 
       return menus;
-
-    }catch(e){
+    } catch (e) {
       logger.warn(`menu not found for site ${site.root.name} error: ${e}`);
       return '';
     }
@@ -90,13 +82,13 @@ export async function getLogoUrl(id: string, liveHostName: string) {
   const logoUrl = `${envProtocol}//${requestUrl}`;
 
   console.log(logoUrl);
-  
+
   const response = await makeRequest({ url: logoUrl });
 
   return response.files?.logo.resourceUrl;
 }
 
-export async function getMenu(liveHostName: string) : Promise<Menu> {
+export async function getMenu(liveHostName: string): Promise<Menu> {
   const requestUrl = `${liveHostName}/api/menus`;
   const envProtocol = new URL(settings.neonFoUrl).protocol;
 
@@ -113,10 +105,13 @@ export type LiveBlogPostsRequestOptions = {
   searchParams: unknown;
 } & AuthenticatedRequestOptions;
 
-export async function getLiveBlogsPosts({ apiHostname, id, searchParams, auth }: LiveBlogPostsRequestOptions) : Promise<{ posts: LiveBlogPost[] }> {
-  const response = await makeRequest(
-    { apiHostname, url: `/api/v2/liveblogs/${id}/posts?${searchParams}`, auth }
-  );
+export async function getLiveBlogsPosts({
+  apiHostname,
+  id,
+  searchParams,
+  auth,
+}: LiveBlogPostsRequestOptions): Promise<{ posts: LiveBlogPost[] }> {
+  const response = await makeRequest({ apiHostname, url: `/api/v2/liveblogs/${id}/posts?${searchParams}`, auth });
 
   // Build an array of LiveBlogPost instances from response.posts
   const posts: LiveBlogPost[] = Array.isArray(response.posts)
