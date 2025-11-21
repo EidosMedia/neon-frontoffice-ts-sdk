dd7158661c5c60093a2fcac48ec0ff0b391b03d8
Valerio Cassani <valerio.cassani@eidosmedia.com>
2025-11-21 14:59:41 +0100

    NEON: updated dependencies

--- src/conf/versions.ts
+++ src/conf/versions.ts
@@ -1,1 +1,1 @@
-export const VERSIONS: string[] = ['NEON.2025.12-SNAPSHOT', 'NEON.2025.12'];
+export const VERSIONS: string[] = ['NEON.2025.10-SNAPSHOT', 'NEON.2025.10', 'NEON.2025.12-SNAPSHOT', 'NEON.2025.12'];
